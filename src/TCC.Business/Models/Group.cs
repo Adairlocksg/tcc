a5a5e7f48733ebf91e2ca8782948053f967a9c35
adair.grassi <adair.grassi@useall.com.br>
2025-02-25 13:25:07 -0300

    feat/create initial entities

---
+++ src/TCC.Business/Models/Group.cs
@@ -0,0 +1,8 @@
+﻿namespace TCC.Business.Models
+{
+    public class Group : Entity
+    {
+        public string Description { get; set; } = string.Empty;
+        public bool Active { get; set; }
+    }
+}
