821f563f123eae458d46f129d53f0299a9e335d8
adair.grassi <adair.grassi@useall.com.br>
2025-03-17 13:29:48 -0300

    feat/initial api configuration

--- src/TCC.Data/Context/MyDbContext.cs
+++ src/TCC.Data/Context/MyDbContext.cs
@@ -7,14 +7,15 @@ namespace TCC.Data.Context
     {
         public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
         {
-            //configs
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
         }
 
         public DbSet<User> Users { get; set; }
         public DbSet<Group> Groups { get; set; }
         public DbSet<UserGroup> UserGroups { get; set; }
 
-        public override int SaveChanges()
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
             foreach (var entry in ChangeTracker.Entries<Entity>())
             {
@@ -26,27 +27,28 @@ namespace TCC.Data.Context
 
                 if (entry.State == EntityState.Modified)
                 {
+                    entry.Property(x => x.CreatedAt).IsModified = false;
                     entry.Entity.UpdatedAt = DateTime.Now;
                 }
             }
 
-            return base.SaveChanges();
+            return base.SaveChangesAsync(cancellationToken);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetProperties()).Where(p => p.ClrType == typeof(string)))
+            {
+                property.SetMaxLength(100);
+            }
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MyDbContext).Assembly);
+
             foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
             {
                 relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
             }
 
-            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MyDbContext).Assembly);
-
-            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetProperties()).Where(p => p.ClrType == typeof(string)))
-            {
-                property.SetMaxLength(100);
-            }
-             
             base.OnModelCreating(modelBuilder);
         }
     }
